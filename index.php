e69deda1839bb74b53c7a9d15084ec37d04cc442
joey <joey.novak@gmail.com>
2016-09-08 20:48:39 -0700

    Initial Commits

---
+++ index.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <script src="js/svg.js"></script>
+    <script src="js/turtle.js"></script>
+    <script   src="//code.jquery.com/jquery-1.12.4.js"   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="   crossorigin="anonymous"></script>
+    <script>
+        jQuery(function(){
+            var turtle = new Turtle(this.getElementById('svg'));
+            turtle.up();
+            turtle.down();
+            turtle.fw(100);
+            turtle.left(90);
+            turtle.fw(100);
+            turtle.circle(200);
+            turtle.arc(98, -180);
+        });
+    </script>
+    <style>
+      line, circle{
+          stroke: black;
+          fill: none;
+      }
+    </style>
+</head>
+<body>
+    <div id="svg-container" style="width: 1000px; height: 1000px">
+        <svg
+            xmlns:dc="http://purl.org/dc/elements/1.1/"
+            xmlns:cc="http://creativecommons.org/ns#"
+            xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+            xmlns:svg="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+            xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+            width="210mm"
+            height="297mm"
+            viewBox="0 0 744.09448819 1052.3622047"
+            id="svg"
+            version="1.1"
+            inkscape:version="0.91 r13725"
+            sodipodi:docname="drawing-1.svg">
+            <defs
+                id="defs6058" />
+            <sodipodi:namedview
+                id="base"
+                pagecolor="#ffffff"
+                bordercolor="#666666"
+                borderopacity="1.0"
+                inkscape:pageopacity="0.0"
+                inkscape:pageshadow="2"
+                inkscape:zoom="0.35"
+                inkscape:cx="-39.285714"
+                inkscape:cy="520"
+                inkscape:document-units="mm"
+                inkscape:current-layer="main"
+                showgrid="false"
+                inkscape:window-width="1680"
+                inkscape:window-height="901"
+                inkscape:window-x="0"
+                inkscape:window-y="1"
+                inkscape:window-maximized="1" />
+            <metadata
+                id="metadata6061">
+                <rdf:RDF>
+                    <cc:Work
+                        rdf:about="">
+                        <dc:format>image/svg+xml</dc:format>
+                        <dc:type
+                            rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                        <dc:title></dc:title>
+                    </cc:Work>
+                </rdf:RDF>
+            </metadata>
+            <g
+                inkscape:label="Layer 1"
+                inkscape:groupmode="layer"
+                id="layer1" />
+        </svg>
+
+    </div>
+</body>
+</html>
---
+++ js/turtle.js
@@ -0,0 +1,92 @@
+/**
+ * Created by joey on 7/13/16.
+ */
+Math.radians = function(degrees){
+    return degrees / 180 * 3.1415927;
+}
+
+Math.degrees = function(radians){
+    return radians * 180 / 3.1415927;
+}
+
+var Turtle = function(element){
+
+    this.svg = new SVG(element);
+    this.svg = this.svg.group();
+    this.isdown = true;
+
+    this.angle = 0;
+    this.x = 0;
+    this.y = 0;
+
+    this.scale = 3.5433;
+
+    this.pi = 3.14159265359;
+
+    this.up = function(){
+        this.isdown = false;
+    }
+
+    this.down = function(){
+        this.isdown = true;
+    }
+
+    this.fw = function(distance){
+        var newx = this.x + (Math.sin(this.angle) * distance * this.scale);
+        var newy = this.y + (Math.cos(this.angle) * distance * this.scale);
+        if(this.isdown)
+            this.svg.line(this.x, this.y, newx, newy);
+        this.x = newx;
+        this.y = newy;
+    }
+
+    this.left = function(degrees){
+        this.angle += Math.radians(degrees);
+    }
+
+    this.right = function(degrees){
+        this.angle -= Math.radians(degrees);
+    }
+
+    this.circle = function(diameter){
+        if(this.isdown){
+            var circle = this.svg.circle(diameter * this.scale);
+            circle.translate(this.x - diameter / 2 * this.scale, this.y - diameter / 2 * this.scale);
+        }
+    }
+
+    this.arc = function(radius, degrees){
+        if(this.isdown){
+            var path = this.svg.path(describeArc(this.x, this.y, radius * this.scale, Math.degrees(this.angle), degrees + Math.degrees(this.angle)));
+            path.fill('none');
+            path.stroke({ color: '#000'});
+            //var ellipse = this.svg.ellipse();
+            //ellipse.radius(a, b * 100);
+            //ellipse.translate(this.x, this.y);
+        }
+    }
+
+    function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+        var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+        return {
+            x: centerX + (radius * Math.cos(angleInRadians)),
+            y: centerY + (radius * Math.sin(angleInRadians))
+        };
+    }
+
+    function describeArc(x, y, radius, startAngle, endAngle){
+
+        var start = polarToCartesian(x, y, radius, endAngle);
+        var end = polarToCartesian(x, y, radius, startAngle);
+
+        var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+        var d = [
+            "M", start.x, start.y,
+            "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+        ].join(" ");
+
+        return d;
+    }
+};
